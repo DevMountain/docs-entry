582e78e16f00c41e8d33aac9c664528abfd2e12b
zach <zaspringer@gmail.com>
2019-02-15 09:05:59 -0700

    finished the code completely. Now just need to get everything pushed up and hosted

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 const express = require('express');
+require('dotenv').config()
+
+let {SURVEY_PATH, DEVMTNWWW_PATH, DEVMOUNTAIN_WEBSITE_PATH} = process.env
 
 const app = express();
 
 app.use("/", express.static(__dirname + "/assets"));
-app.use("/surveys", express.static(__dirname + "/../api-documenation/surveys/build/"));
-app.use("/devmtnwww", express.static(__dirname + "/../api-documenation/devmtnwww/build/"));
-app.use("/Devmountainwebsite", express.static(__dirname + "/../api-documenation/Devmountainwebsite/build/"));
+app.use("/surveys", express.static(__dirname + SURVEY_PATH));
+app.use("/devmtnwww", express.static(__dirname + DEVMTNWWW_PATH));
+app.use("/Devmountainwebsite", express.static(__dirname + DEVMOUNTAIN_WEBSITE_PATH));
 
 app.get('/surveys', (req, res) => {
     let options = { 
-        root: __dirname + '/../api-documenation/surveys/build/'
+        root: __dirname + SURVEY_PATH
     }
     res.sendFile('index.html', options)
 })
 
 app.get('/devmtnwww', (req, res) => {
     let options = { 
-        root: __dirname + '/../api-documenation/devmtnwww/build/'
+        root: __dirname + DEVMTNWWW_PATH
     }
     res.sendFile('index.html', options)
 })
 
 app.get('/Devmountainwebsite', (req, res) => {
     let options = { 
-        root: __dirname + '/../api-documenation/Devmountainwebsite/build/'
+        root: __dirname + DEVMOUNTAIN_WEBSITE_PATH
     }
     res.sendFile('index.html', options)
 })
