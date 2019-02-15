d05d13497ea75ac1b0abe61384066b74f22e1b4c
zach <zaspringer@gmail.com>
2019-02-14 17:24:07 -0700

    all the code is added

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ const express = require('express');
 
 const app = express();
 
-// app.use(express.static('assets'))
 app.use("/", express.static(__dirname + "/assets"));
-// app.use("/surveys", express.static(__dirname + "/build"));
 app.use("/surveys", express.static(__dirname + "/../api-documenation/surveys/build/"));
+app.use("/devmtnwww", express.static(__dirname + "/../api-documenation/devmtnwww/build/"));
+app.use("/Devmountainwebsite", express.static(__dirname + "/../api-documenation/Devmountainwebsite/build/"));
 
 app.get('/surveys', (req, res) => {
     let options = { 
         root: __dirname + '/../api-documenation/surveys/build/'
+    }
+    res.sendFile('index.html', options)
+})
+
+app.get('/devmtnwww', (req, res) => {
+    let options = { 
+        root: __dirname + '/../api-documenation/devmtnwww/build/'
+    }
+    res.sendFile('index.html', options)
+})
+
+app.get('/Devmountainwebsite', (req, res) => {
+    let options = { 
+        root: __dirname + '/../api-documenation/Devmountainwebsite/build/'
     }
     res.sendFile('index.html', options)
 })
